c2538adc4bf1f5c2ef76bdf56a70784399debf26
Tony Coop <tony@thewholeworldwindow.co.uk>
2017-11-22 18:00:31 +0800

    Added mongoose and a test models implementation.

---
+++ models/todo.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+const mongooseStringQuery = require('mongoose-string-query')
+const timestamps = require('mongoose-timestamp')
+
+const TodoSchema = new mongoose.Schema(
+  {
+    task: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    status: {
+      type: String,
+      required: true,
+      enum: ['pending', 'complete', 'in progress', 'overdue'],
+      default: 'pending'
+    }
+  },
+  { minimize: false }
+)
+
+TodoSchema.plugin(timestamps)
+TodoSchema.plugin(mongooseStringQuery)
+
+module.exports = mongoose.model('Todo', TodoSchema)
